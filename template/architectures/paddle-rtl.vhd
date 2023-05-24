c53a01082e80a617b2c7990639b85d66c8188981
Roman Batut <77281414+Roman-batut@users.noreply.github.com>
2023-05-24 15:58:51 +0200

    etape 3 2

--- template/architectures/paddle-rtl.vhd
+++ template/architectures/paddle-rtl.vhd
@@ -1,5 +1,9 @@
 ARCHITECTURE rtl OF paddle IS
+signal paddle_loc : std_logic_vector(3 downto 0) := "0101";
 BEGIN
+
+-- Paddle location
+paddle_loc_x <= paddle_loc;
 
 control : process (clk, n_reset)
 begin
@@ -8,26 +12,23 @@ begin
         -- Reset
         if n_reset = '0' then
             
-            paddle_loc_x <= "0101";
+            paddle_loc <= "0101";
 
         elsif game_ctl = '1' then
 
             -- Right shift
-            if but_right = '1' and to_integer(signed(paddle_loc_x)) < 8 then
-                paddle_loc_x <= paddle_loc_x + 1;
+            if but_right = '1' and to_integer(signed(paddle_loc)) < 8 then
+                paddle_loc <= to_integer(signed(paddle_loc)) + 1;
             end if;
 
             -- Left shift
-            if but_left = '1' and to_integer(signed(paddle_loc_x)) > 3 then
-                paddle_loc_x <= paddle_loc_x - 1;
+            if but_left = '1' and to_integer(signed(paddle_loc)) > 3 then
+                paddle_loc <= to_integer(signed(paddle_loc)) - 1;
             end if;
 
         end if;
             
     end if;
 end process;
-    
-end process;
-
 
 END rtl;

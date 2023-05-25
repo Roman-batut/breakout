0ef952ec3a644d156517fd110e064d90c30ce7c1
Roman Batut <77281414+Roman-batut@users.noreply.github.com>
2023-05-25 19:12:52 +0200

    etape 5 12

--- template/architectures/game_ctl-rtl.vhd
+++ template/architectures/game_ctl-rtl.vhd
@@ -1,11 +1,11 @@
 ARCHITECTURE rtl OF game_ctl IS
-signal play_without_bricks : std_logic;
+signal play_without_bricks : std_logic := '0';
 
 type state_type is (play, end_stop, idle_stop);
-    signal s_state : state_type := idle_stop;
+    signal s_state, s_next_state : state_type := idle_stop;
 BEGIN
 
-game : process(clk, n_reset, but_left, but_right, ball_loc_invalid, brick_loc_valid)
+next_state : process(clk, n_reset, but_left, but_right, ball_loc_invalid, brick_loc_valid)
 begin
 
     if n_reset = '0' then
@@ -24,19 +24,25 @@ begin
                 end if;
 
                 if (but_left = '1') or (but_right = '1') then
-                    s_state <= play;
+                    s_next_state <= play;
                 end if;
 
             -- Play State
             when play =>
                 if (ball_loc_invalid = '1') or ((brick_loc_valid = "0000") and play_without_bricks = '0') then
-                    s_state <= end_stop;
+                    s_next_state <= end_stop;
                 end if;
             
             -- End Stop State
             when others =>
-                s_state <= idle_stop; 
+                s_next_state <= idle_stop; 
         end case;
+    end if;
+
+    if s_next_state = play then
+        s_state <= s_next_state;
+    else
+        game_ctl_out <= '0';
     end if;
 
 end process;
